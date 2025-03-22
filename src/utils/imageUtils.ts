e4847ab74a0c6e98f9da8807df18133a4009ae55
Sitaram Oli <sitaramoli1998@gmail.com>
2025-03-22 22:21:43 +0545

    Image Compressor added

---
+++ src/utils/imageUtils.ts
@@ -0,0 +1,11 @@
+export function formatFileSize(bytes: number) {
+    if (bytes === 0) return '0 Bytes';
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(1)) + ' ' + sizes[i];
+}
+
+export function calculateReduction(original: number, compressed: number) {
+    return Math.round((1 - compressed / original) * 100);
+}
